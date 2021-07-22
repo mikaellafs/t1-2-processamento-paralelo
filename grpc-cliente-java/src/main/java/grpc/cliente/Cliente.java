35f4390f7ec93897f9e16d865d62797efd3a631c
Leonardo Deorce <leodeorce@gmail.com>
2021-07-22 15:27:35 -0300

    Início da implementação do cliente java

---
+++ grpc-cliente-java/src/main/java/grpc/cliente/Cliente.java
@@ -0,0 +1,11 @@
+package grpc.cliente;
+
+public final class Cliente {
+    
+    private Cliente() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
