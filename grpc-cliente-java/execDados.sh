555330f9a4bdb2e489fa9ae14ac7fb1d82d42c33
Leonardo Deorce <leodeorce@gmail.com>
2021-07-27 12:03:16 -0300

    Fix script regex

--- grpc-cliente-java/execDados.sh
+++ grpc-cliente-java/execDados.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cd target/
+
 for i  in  1 2 4 8; do
 	CONTADOR=0
 	TEMPO_MEDIO=0.0
 	while [  $CONTADOR -lt 10 ]; do
 		result=`java -jar grpc-cliente-java-1.0-SNAPSHOT-jar-with-dependencies.jar $i $1` 
-		tempo=`echo "${result}" | grep -oP '[0-9]+.[0-9]+'`
+		tempo=`echo "${result}" | grep -oP '[0-9]+\.[0-9]+'`
+		#echo ${tempo}
 
 		TEMPO_MEDIO=$(bc -l <<<"${tempo}/10 + ${TEMPO_MEDIO}");
 		
