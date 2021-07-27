105807e769dae2736fb681198bfb83ac07177e33
Mikaella <42984505+mikaellafs@users.noreply.github.com>
2021-07-27 03:14:23 -0300

    script pra rodar experimentos automaticamente

---
+++ grpc-cliente-java/execDados.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i  in  1 2 4 8; do
+	CONTADOR=0
+	TEMPO_MEDIO=0.0
+	while [  $CONTADOR -lt 10 ]; do
+		result=`java -jar grpc-cliente-java-1.0-SNAPSHOT-jar-with-dependencies.jar $i $1` 
+		tempo=`echo "${result}" | grep -oP '[0-9]+.[0-9]+'`
+
+		TEMPO_MEDIO=$(bc -l <<<"${tempo}/10 + ${TEMPO_MEDIO}");
+		
+		let CONTADOR=CONTADOR+1; 
+	done
+	echo "i=$i : $TEMPO_MEDIO"
+done
