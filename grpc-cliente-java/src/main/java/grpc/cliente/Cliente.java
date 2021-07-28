d8b9c6d2d41a796578ad6df069964144796f4b67
Leonardo Deorce <leodeorce@gmail.com>
2021-07-27 22:51:18 -0300

    Comentários no cliente + deletando teste_client.py

--- grpc-cliente-java/src/main/java/grpc/cliente/Cliente.java
+++ grpc-cliente-java/src/main/java/grpc/cliente/Cliente.java
@@ -1,7 +1,7 @@
 /**
  *  Se alguns nomes não forem resolvidos, executar "mvn compile" em /grpc-cliente-java/
  *  e garantir que target/generated-sources/protobuf/java e target/generated-sources/protobuf/grpc-java
- *  são vistos como source roots pela IDE (extensão para Maven, por exemplo).
+ *  são vistos como source roots pela IDE (uso de extensão para Maven, por exemplo).
  */
 
 package grpc.cliente;
@@ -21,11 +21,15 @@ public final class Cliente implements Runnable
 {
     private int m;
     private String serverAddress = "localhost:50051";
+
+    // Objeto para a classe gerada pelo compilador de proto3 usado pelo gRPC
     private DataStorageGrpc.DataStorageBlockingStub blockingStub;
 
     private Cliente( int m )
     {
         this.m = m;
+
+        // Um channel estabelece uma conexão entre cliente e servidor
         ManagedChannel channel = ManagedChannelBuilder.forTarget(serverAddress).usePlaintext().build();
         blockingStub = DataStorageGrpc.newBlockingStub(channel);
     }
@@ -35,31 +39,48 @@ public final class Cliente implements Runnable
     {
         List<Key> keys = new ArrayList<Key>(m);
         Random rand = new Random();
+
         for( int i = 1; i <= m; i++ ) {
+            // Um número inteiro aleatório de 0 a 10.000.000 é criado e usado como parâmetro
+            // para o método put() no stub, o que executa uma chamada remota ao servidor
             keys.add( blockingStub.put(Data.newBuilder().setNum(rand.nextInt(10_000_000)).build()) );
         }
+
         for( Key key : keys ) {
-            blockingStub.get(key).getNum(); // Referente ao get sem fazer print nas linhas
+            // Para cada chave de cada número gerado anteriormente, executa o método get() no
+            // stub, o que executa uma chamada remota ao servidor
+            blockingStub.get(key).getNum();
         }
     }
 
+    /**
+     * Executa i threads Cliente que inserem m números na tabela hash do servidor usando gRPC.
+     * 
+     * @param args Array cuja primeira posição é a quantidade de threads Cliente a serem executadas e
+     * cuja segunda posição é a quantidade de números a serem inseridos na tabela hash do servidor.
+     */
     public static void main( String[] args )
     {
         final int numeroClientes = Integer.parseInt(args[0]);
-        final int m = Integer.parseInt(args[1]);       
+        final int m = Integer.parseInt(args[1]);
         
         System.out.println("Clientes = " + args[0] + " - Números = " + args[1]);
+
         ArrayList<Thread> threads = new ArrayList<>();
         Cliente[] clientes = new Cliente[numeroClientes];
         
+        // Inicia contagem de tempo
         long tempoInicial = System.nanoTime();
+
         for( Cliente cliente : clientes ) {
-            cliente = new Cliente(m/numeroClientes);
+            // Divide os m números igualmente entre as i threads
+            cliente = new Cliente(m / numeroClientes);
             Thread thread = new Thread(cliente);
             threads.add(thread);
             thread.start();
         }
         
+        // Assegura a finalização de todas as threads
         for( Thread thread : threads ) {
             try {
                 thread.join();
@@ -70,8 +91,12 @@ public final class Cliente implements Runnable
         
         threads.clear();
         
+        // Para a contagem de tempo
         long tempoFinal = System.nanoTime();
+
+        // Conversão de nanossegundos para segundos
         double tempo = (double) (tempoFinal - tempoInicial) / 1_000_000_000;
+
         System.out.println("Tempo Gasto = " + tempo + " s");
     }
 }
